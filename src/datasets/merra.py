03da0b740ee4c24a5c01803ffe243ef49685c419
Kostas Andreadis <kandread@jpl.nasa.gov>
2016-03-14 09:55:46 -0700

    Fixes #20. Add runid as part of the MERRA filename.

--- src/datasets/merra.py
+++ src/datasets/merra.py
@@ -21,13 +21,27 @@ def dates(dbname):
     return dts
 
 
+def _merraRunid(year):
+    """MERRA files use different run IDs depending on the timestamp year."""
+    if year < 1992:
+        runid = 100
+    elif year > 1991 and year < 2001:
+        runid = 200
+    elif year > 2000 and year < 2011:
+        runid = 300
+    else:
+        runid = 400
+    return runid
+
+
 def _downloadVariable(varname, dbname, dts, bbox):
     """Download specific variable from the MERRA Reanalysis dataset."""
     # FIXME: Grid is not rectangular, but 0.5 x 0.625 degrees
     res = 0.5
     for ts in [dts[0] + timedelta(dti) for dti in range((dts[1] - dts[0]).days + 1)]:
         try:
-            url = "http://goldsmr4.sci.gsfc.nasa.gov:80/opendap/MERRA2/M2T1NXSLV.5.12.4/{0}/{1:02d}/MERRA2_400.tavg1_2d_slv_Nx.{0:04d}{1:02d}{2:02d}.nc4".format(ts.year, ts.month, ts.day)
+            runid = _merraRunid(ts.year)
+            url = "http://goldsmr4.sci.gsfc.nasa.gov:80/opendap/MERRA2/M2T1NXSLV.5.12.4/{1}/{2:02d}/MERRA2_{0}.tavg1_2d_slv_Nx.{1:04d}{2:02d}{3:02d}.nc4".format(runid, ts.year, ts.month, ts.day)
             ds = netcdf.Dataset(url)
             lat = ds.variables["lat"][:]
             lon = ds.variables["lon"][:]
@@ -35,26 +49,14 @@ def _downloadVariable(varname, dbname, dts, bbox):
             i1, i2, j1, j2 = datasets.spatialSubset(lat, lon, res, bbox)
             lat = lat[i1:i2]
             lon = lon[j1:j2]
-            # if bbox is not None:
-            #     i = np.where(np.logical_and(lat > bbox[1], lat < bbox[3]))[0]
-            #     j = np.where(np.logical_and(lon > bbox[0], lon < bbox[2]))[0]
-            #     lat = lat[i]
-            #     lon = lon[j]
-            # else:
-            #     i = range(len(lat))
-            #     j = range(len(lon))
-            # data = np.zeros((len(i), len(j)))
             data = np.zeros((i2-i1, j2-j1))
             if varname == "tmax":
-                # hdata = ds.variables["T2M"][:, i, j]
                 hdata = ds.variables["T2M"][:, i1:i2, j1:j2]
                 data = np.amax(hdata, axis=0) - 273.15
             elif varname == "tmin":
-                # hdata = ds.variables["T2M"][:, i, j]
                 hdata = ds.variables["T2M"][:, i1:i2, j1:j2]
                 data = np.amin(hdata, axis=0) - 273.15
             elif varname in ["wind"]:
-                # hdata = np.sqrt(ds.variables["U10M"][:, i, j]**2 + ds.variables["V10M"][:, i, j]**2)
                 hdata = np.sqrt(ds.variables["U10M"][:, i1:i2, j1:j2]**2 + ds.variables["V10M"][:, i1:i2, j1:j2]**2)
                 data = np.mean(hdata, axis=0)
             filename = dbio.writeGeotif(lat, lon, res, data)
