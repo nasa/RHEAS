d93e2acd1e664c66488cee9f5ac4573375f56326
Kostas Andreadis <kandread@jpl.nasa.gov>
2016-08-01 12:47:24 -0700

    Added separate options for DSSAT assimilation.

--- src/dssat.py
+++ src/dssat.py
@@ -69,8 +69,16 @@ def _run1(modelpath, exe, assimilate="Y"):
     # devnull = open(os.devnull, 'wb')
     # subprocess.call("wine {0} SOIL_MOISTURE.ASC LAI.txt SM{1} LAI{1}".format(exe, assimilate), shell=True)#, stdout=subprocess.PIPE, stderr=devnull)
     # devnull.close()
+    if assimilate.lower() == "sm" or assimilate.lower() == "y":
+        sm_assim = "Y"
+    else:
+        sm_assim = "N"
+    if assimilate.lower() == "lai" or assimilate.lower() == "y":
+        lai_assim = "Y"
+    else:
+        lai_assim = "N"
     subprocess.call(["wine", exe, "SOIL_MOISTURE.ASC", "LAI.txt",
-                     "SM{0}".format(assimilate), "LAI{0}".format(assimilate)])
+                     "SM{0}".format(sm_assim), "LAI{0}".format(lai_assim)])
 
 
 class DSSAT:
