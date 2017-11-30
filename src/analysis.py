b43f2879e726f8fd5ea84c1831eb2af7a1badf14
Kostas Andreadis <kandread@jpl.nasa.gov>
2017-11-30 08:34:09 -0800

    Fixed bugs in crop yield exctraction to shapefile.

--- src/analysis.py
+++ src/analysis.py
@@ -16,26 +16,28 @@ from datetime import datetime
 def cropYield(shapefile, name, startdate="", enddate="", crop="maize", dbname="rheas"):
     """Extract crop yield from a specified simulation *name* for dates ranging
     from *startdate* to *enddate*, and saves them a *shapefile*."""
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
     log = logging.getLogger(__name__)
     db = dbio.connect(dbname)
     cur = db.cursor()
     datesql = ""
-    try:
-        sdt = datetime.strptime(startdate, "%Y-%m-%d")
-        datesql = "and fdate>=date'{0}'".format(sdt.strftime("%Y-%m-%d"))
-    except:
-        log.warning("Start date is invalid and will be ignored.")
-    try:
-        edt = datetime.strptime(enddate, "%Y-%m-%d")
-        datesql += "and fdate<=date'{0}'".format(edt.strftime("%y-%m-%d"))
-    except:
-        log.warning("End date is invalid and will be ignored.")
-    datesql = "and fdate>=date'{0}' and fdate<=date'{1}'"
+    if len(startdate) > 0:
+        try:
+            sdt = datetime.strptime(startdate, "%Y-%m-%d")
+            datesql = "and fdate>=date'{0}'".format(sdt.strftime("%Y-%m-%d"))
+        except:
+            log.warning("Start date is invalid and will be ignored.")
+    if len(enddate) > 0:
+        try:
+            edt = datetime.strptime(enddate, "%Y-%m-%d")
+            datesql += "and fdate<=date'{0}'".format(edt.strftime("%y-%m-%d"))
+        except:
+            log.warning("End date is invalid and will be ignored.")
     fsql = "with f as (select gid,geom,gwad,ensemble,fdate from (select gid,geom,gwad,ensemble,fdate,row_number() over (partition by gid,ensemble order by gwad desc) as rn from {0}.dssat) gwadtable where rn=1 {1})".format(name, datesql)
     sql = "{0} select gid,st_astext(geom),max(gwad) as max_yield,avg(gwad) as avg_yield,stddev(gwad) as std_yield,max(fdate) as fdate from f group by gid,geom".format(fsql)
     cur.execute(sql)
     if bool(cur.rowcount):
-        results = cur.fecthall()
+        results = cur.fetchall()
         drv = ogr.GetDriverByName("ESRI Shapefile")
         ds = drv.CreateDataSource(shapefile)
         lyr = ds.CreateLayer("yield", geom_type=ogr.wkbMultiPolygon)
