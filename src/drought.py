c0ff8b68bd34ece5e5f3200a6549aee9f9067eef
Kostas Andreadis <kandread@jpl.nasa.gov>
2018-05-08 13:13:37 -0700

    Initial version of Combined Drought Index calculation.

--- src/drought.py
+++ src/drought.py
@@ -30,6 +30,88 @@ def _movingAverage(data, n):
     return out[n - 1:] / n
 
 
+def _calcSuctionHead(model, cid, nlayers=3):
+    """Calculate soil suction from soil moisture using the Clapp
+    and Hornberger (1978) model and parameters."""
+    Ksat = np.array([63.36, 56.16, 12.49, 2.59, 2.5, 2.27, 0.612, 0.882, 0.781, 0.371, 0.461])
+    Ksat /= (10 * 24.)  # convert from cm/hr to mm/day
+    n = [.395, .41, .435, .485, .451, .42, .477, .476, .426, .492, .482]
+    psi_a = [121., 90., 218., 786., 478., 299., 356., 630., 153., 490., 405.]
+    b = [4.05, 4.38, 4.9, 5.3, 5.39, 7.12, 7.75, 8.52, 10.4, 10.4, 11.4]
+    # get saturated conductivity values to identify soil type
+    db = dbio.connect(model.dbname)
+    cur = db.cursor()
+    cur.execute("select line from vic.soils where id={0}".format(cid))
+    line = cur.fetchone()[0]
+    k = np.mean(np.array(map(float, line.split()[9+nlayers:nlayers+11])))
+    z = sum(map(float, line.split()[4*nlayers+10:4*nlayers+12])) * 1000.
+    ki = np.argmin(abs(Ksat - k))
+    # get soil moisture for surface and root zone layer
+    sql = "select fdate,sum(st_value(rast,geom)) from {0}.soil_moist, {0}.basin where st_intersects(rast,geom) and gid={1} and layer<3 group by fdate order by fdate".format(model.name, cid)
+    cur.execute(sql)
+    if bool(cur.rowcount):
+        results = cur.fetchall()
+        sm = pandas.Series([r[1] for r in results], np.array([r[0] for r in results], dtype='datetime64'))
+        # convert into dekad averages
+        d = sm.index.day - np.clip((sm.index.day-1) // 10, 0, 2)*10 - 1
+        date = sm.index.values - np.array(d, dtype='timedelta64[D]')
+        sm = sm.groupby(date).mean()
+        # calculate soil suction
+        pf = np.log(psi_a[ki] * ((sm / z) / n[ki])**(-b[ki]))
+        # calculate z-score of soil suction
+        st = "{0}-{1}-{2}".format(model.startyear, model.startmonth, model.startday)
+        et = "{0}-{1}-{2}".format(model.endyear, model.endmonth, model.endday)
+        pfz = (pf[st:et] - pf.mean()) / pf.std()
+        pfz = pfz.resample('D').ffill().values
+    else:
+        pfz = None
+    cur.close()
+    db.close()
+    return pfz
+
+
+def _calcFpar(model, cid):
+    """Retrieve the Photosynthetically Active Radiation from the model simulation."""
+    db = dbio.connect(model.dbname)
+    cur = db.cursor()
+    sql = "select fdate,st_value(rast,geom) from {0}.par, {0}.basin where st_intersects(rast,geom) and gid={1} order by fdate".format(model.name, cid)
+    cur.execute(sql)
+    if bool(cur.rowcount):
+        results = cur.fetchall()
+        fpar = pandas.Series([r[1] for r in results], np.array([r[0] for r in results], dtype='datetime64'))
+        d = fpar.index.day - np.clip((fpar.index.day-1) // 10, 0, 2)*10 - 1
+        date = fpar.index.values - np.array(d, dtype='timedelta64[D]')
+        fpar = fpar.groupby(date).mean()
+        st = "{0}-{1}-{2}".format(model.startyear, model.startmonth, model.startday)
+        et = "{0}-{1}-{2}".format(model.endyear, model.endmonth, model.endday)
+        fparz = (fpar[st:et] - fpar.mean()) / fpar.std()
+        fparz = fparz.resample('D').ffill().values
+    else:
+        fparz = None
+    cur.close()
+    db.close()
+    return fparz
+
+
+def calcCDI(model, cid):
+    """Calculate Combined Drought Index as a monthly time series. The index is
+    categorical with the values corresponding to:
+    0 = No drought
+    1 = Watch (Precipitation deficit)
+    2 = Warning (Soil moisture deficit)
+    3 = Alert 1 (Vegetation stress following precipitation deficit)
+    4 = Alert 2 (Vegetation stress following precipitation/soil moisture deficit)."""
+    spi = calcSPI(3, model, cid)
+    sma = _calcSuctionHead(model, cid)
+    fapar = _calcFpar(model, cid)
+    cdi = np.zeros(len(spi), dtype='int')
+    cdi[spi < -1] = 1
+    cdi[fapar > 1 & spi < -1] = 2
+    cdi[fapar < -1 & spi < -1] = 3
+    cdi[fapar < -1 & sma > 1 & spi < -1] = 4
+    return cdi
+
+
 def calcSRI(duration, model, cid):
     """Calculate Standardized Runoff Index for specified month
     *duration*."""
@@ -191,6 +273,8 @@ def calc(varname, model, cid):
         output = calcSRI(duration, model, cid)
     elif varname == "severity":
         output = calcSeverity(model, cid)
+    elif varname == "cdi":
+        output = calcCDI(model, cid)
     elif varname == "smdi":
         output = calcSMDI(model, cid)
     elif varname == "dryspells":
